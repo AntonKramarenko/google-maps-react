567c2fc3c271585588d55a3ffe7a2ee3d8ce2945
Anton <kramarenko4362@gmail.com>
2022-07-14 10:40:44 +0300

    add google maps screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { Map } from './components/Map'
+import { useJsApiLoader } from '@react-google-maps/api';
 import './App.css';
 const API_KEY = process.env.REACT_APP_API_KEY
+console.log(API_KEY);
 
+
+const defaultCenter = {
+  lat: 51.509865,
+  lng: -0.118092
+};
+
+const libraries = ['places']
 
 const App = () => {
-  console.log(API_KEY);
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: API_KEY,
+    libraries
+  })
   return (
     <div className="App">
-      test
+      {isLoaded ? <Map center={defaultCenter} /> : <h2>Loading</h2>}
     </div>
   );
 }
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useRef } from 'react'
+import { GoogleMap } from '@react-google-maps/api';
+import s from './Map.module.css'
+
+
+const containerStyle = {
+    width: '100%',
+    height: '100vh'
+  };
+  
+const defaultOptions ={
+    panControl: true,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    clickableIcons:false,
+    keyboardShortcuts: false,
+    scrollwheel:false,
+    disableDoubleClickZoom: false,
+    fullscreenControl: false
+  }
+
+export const Map = ({center}) => {
+
+    const mapRef = useRef(undefined)
+
+    const onLoad = useCallback(function callback(map) {
+        mapRef.current = map
+      }, [])
+    
+      const onUnmount = useCallback(function callback(map) {
+        mapRef.current = undefined
+      }, [])
+
+
+
+  return (
+    <div className={s.contaiter}>
+         <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={defaultOptions}
+      >
+      </GoogleMap>
+    </div>
+  )
+}
