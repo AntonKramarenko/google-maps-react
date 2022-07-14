afd189838541bd557d5a09f40e430854880718cd
Anton <kramarenko4362@gmail.com>
2022-07-14 09:38:37 +0300

    clear project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+const API_KEY = process.env.REACT_APP_API_KEY
+
+
+const App = () => {
+  console.log(API_KEY);
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
